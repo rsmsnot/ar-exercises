bdb4f09a7c94b8f9e78ba3a0effb00733ba3153a
rsmsnot <rsmsnot@outlook.com>
2019-07-09 20:50:20 +0000

    exercise 7 complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Please enter the name of a store"
+print '=>'
+@user_store = gets.chomp
+
+new_store = Store.create(name: @user_store)
+
+new_store.valid?
+errors = new_store.errors.messages
+
+errors.each do |error, message|
+    puts "#{error}= #{message.to_s}"
+end
